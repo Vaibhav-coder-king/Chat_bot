4737b26cb71901f70aeef159831340d82c255f14
Vaibhav-Coding-King <vaibhavchopra302@gmail.com>
2025-07-23 11:49:37 +0530

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-api_key="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"
+api_key=""
 #add your OpenAI API key here
 #you can get it from https://platform.openai.com/account/api-keys
